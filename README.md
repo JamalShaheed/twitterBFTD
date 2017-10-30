6024e366a27d928a26374e5dbcf6e1557b4e5967
Jamal Shaheed <jamal.shaheed@gmail.com>
2017-10-30 09:30:23 +0800

    Matching original repo

--- README.md
+++ README.md
@@ -1,41 +1,10 @@
+# twitterBFTD
+Twitter back from the death looks in a user tweets history for domain names that are available for registration.
 
-Hi there, if you have ended up here from a link on Twitter then the website you are looking for does not exist (if you were looking for savesofia.org she got the transplant and is healthy)
-
-If you were the previous owner of the domain send me an email at jamal.shaheed@gmail.com and I can transfer ownership (if the domain wasen't too expensive to purchase you can have it free of charge)
-
-Fo anyone else read below
-
------------------------------------------------------------------------------------------------------------------------------------
-
-The script takes in a list of Twitter handles and dowloads all previous tweets, it then runs a WHOIS check on each domain linked by the tweet, storing the URL to a text file if an entry dosen't exist (meaning the domain has expired/ is avalible for regristration)
+See blogpost: [How I hijacked top celebrities tweets including Katy Perry, Shakira…](https://medium.com/@MisterCh0c/how-i-hijacked-top-celebrities-tweets-including-katy-perry-shakira-fca3a0e751c6#.eddyocox9)
 
 
-This Python script was originally created by [misterch0c](https://github.com/misterch0c) he did all the hard work.
+`pip install pythonwhois tweepy`
 
-This fork has a few changes to the original repo
-
-* More exclusions, read from a csv file
-* The ability to follow referal links (bit.ly, smart.url) recursively
-* Files to be read from/written are now called with commandline arguments instead of hardcoded
-* Some exceptions caught to stop the script terminating before searching all provided twitter handles
-* Domains that are owned are added to the exclusion list to avoid repeated testing
-
-Some features/changes to be made:
-- [ ] add verbose output and quiet output flags 
-- [x] PEP8 compliant formating
-- [ ] Deal with URL's that have a '?' instead of a / 
-- [ ] Get around Twitter API limits when downloading Tweets
-- [ ] Try filter out Tweets with no URL before downloading (might not be possible/ worth it)
-- [ ] Add the ability to check Twitter handle through commandline argument instead of file
-
----------------------------------------------------------------------------------------------------------------------------------
-
-`pip install pythonwhois tweepy requests`
-
-Usage: `python3 twrtBFTD.py -i FILE_WITH_TWITTER_HANDLES -o FILE_WRITE_RESULTS_TO`
-A sample file "accounts" has been provided with the top 1000 Twitter users
-
-Example Usage to use "accounts" file and write to PWND.txt:
-`python3 twrtBFTD.py -i accounts -o PWND.txt`
-
-Got to https://apps.twitter.com/ to create your consumer key/secret and access token/secret and add them to secrets.py
+Usage:
+Just run it at the root of the folder and it will create BFTD_results.txts
--- twrtBFTD.py
+++ twrtBFTD.py
@@ -1,157 +1,113 @@
-
+#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-import requests
+# Copyright (c) 2017 @misterch0c
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the HTL Hodge Twins License as published by
+# the Free Gains Foundation, version 1 and only of the License.
+#
+# This program is distributed in the hope that you do whatever the FUCK,
+# you wanna do with it
+
+
 import tweepy
 import time
 import threading
 import sys
 import re
 import pythonwhois
-import argparse
-from secrets import consumer_key, consumer_secret, access_token, \
-    access_token_secret
+from secrets import consumer_key, consumer_secret, access_token, access_token_secret
 
 
-class myThread(threading.Thread):
-
-    def __init__(self, accounts):
-         threading.Thread.__init__(self)
-         self.accounts = accounts
- 
+class myThread (threading.Thread):
+    def __init__(self,accounts):
+        threading.Thread.__init__(self)
+        self.accounts=accounts
     def run(self):
+        print "Starting " + self.name
         findem(self.accounts)
 
-
 def get_all_tweets(screen_name):
-    alltweets = []
-    new_tweets = api.user_timeline(screen_name=screen_name, count=200)
-    alltweets.extend(new_tweets)
-    oldest = alltweets[-1]['id'] - 1
-    while len(new_tweets) > 0:
-        new_tweets = api.user_timeline(screen_name=screen_name,
-                count=200, max_id=oldest)
-        alltweets.extend(new_tweets)
-        oldest = alltweets[-1]['id'] - 1
-        print ('...%s tweets downloaded so far for %s' \
-            % (len(alltweets), '@' + screen_name))
-    return alltweets
+	alltweets = []
+	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+	alltweets.extend(new_tweets)
+	oldest = alltweets[-1]['id'] - 1
+	while len(new_tweets) > 0:
+		new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+		alltweets.extend(new_tweets)
+		oldest = alltweets[-1]['id'] - 1
+		print "...%s tweets downloaded so far for %s" % ((len(alltweets)),"@"+screen_name)
+	return alltweets
 
 
 def is_not_registred(url):
-    try:
-        who = pythonwhois.get_whois(url)
-        return 'NOT FOUND' in str(who)
-    except Exception as eWho:
-        print (eWho)
-        excluded.append(url)
-        return False
+	try:
+		who=pythonwhois.get_whois(url)
+		return 'NOT FOUND' in str(who)
 
+	except Exception:
+		print('oops')
+		return False
 
-def get_accounts(input_file):
-    acc = []
-    with open(input_file) as f:
-        for l in f.readlines():
-            twit_name = l.split(',')[0]
-            acc.append(twit_name)
-    return acc
+def get_accounts():
+	acc=[]
+	with open('accounts_leftover') as f:
+		for l in f.readlines():
+			twit_name=l.split(',')[0]
+			acc.append(twit_name)
+		return acc
 
 
 def findem(accounts):
-    urls = []
-    lock.acquire()
-    if len(accounts) == 0:
-        lock.release()
-        return
-
-    acc = accounts.pop(0)
-    lock.release()
-    tweets = get_all_tweets(acc)
-    for tweet in tweets:
-        if 'RT' not in tweet['text']:
-            nn = tweet['entities']['urls']
-            for ur in nn:
-                expanded_url = ur['expanded_url']
-                try:
-                    expanded_url = unshorten_url(expanded_url)
-                except Exception as e:
-                    print(e)
-                expanded_url = expanded_url.replace('http://', ''
-                        ).replace('https://', '').replace('www.', ''
-                        ).split('/')[0].split('.')
-
-                expanded_url = expanded_url[len(expanded_url)
-                    - 2:len(expanded_url)]
-
-                expanded_url = '.'.join(x for x in expanded_url)
-
-                if expanded_url.lower() not in excluded:
-                    print ('[' + acc + ']' + '  --  ' + expanded_url)
-                    if is_not_registred(expanded_url):
-                        print ('PWND ' + acc + ' --  ' + expanded_url)
-                        urls.append(expanded_url)
-                    else:
-                        excluded.append(expanded_url)
-                else:
-                    print ('Skipping ' + expanded_url)
-
-    thread1 = myThread(accounts)
-    thread1.daemon = True
-    thread1.start()
-    f = open(output, 'a')
-    f.write(str(urls) + acc + '\n')
-    f.close()
-    print ('+++ ' + str(len(urls)) + ' available domain found +++')
-    print (urls)
+	urls=[]
+	print('++ new thread ++')
+	lock.acquire()
+	if len(accounts) == 0:
+		print("++ OVER ++")
+		lock.release()
+		return
+	acc = accounts.pop(0)
+	lock.release()
+	tweets=get_all_tweets(acc)
+	for tweet in tweets:
+		if 'RT' not in tweet['text']:
+			nn=tweet['entities']['urls']
+			for ur in nn:
+				expanded_url=ur["expanded_url"]
+				expanded_url = expanded_url.replace("http://","").replace("https://","").replace("www.", "").split("/")[0].split(".")
+				expanded_url = expanded_url[len(expanded_url)-2:len(expanded_url)]
+				expanded_url = '.'.join(x for x in expanded_url)
+				if expanded_url.lower() not in excluded:
+					print("["+acc+"]"+"  --  "+ expanded_url)
+					if is_not_registred(expanded_url):
+						print("PWND " + acc +" --  "+expanded_url)
+						urls.append(expanded_url)
+	thread1=myThread(accounts)
+	thread1.daemon=True
+	thread1.start()
+	f = open('BFTD_results.txt', 'a')
+	f.write(str(urls) + acc +'\n')
+	f.close() 
+	print("+++ " +str(len(urls))+ " available domain found +++")
+	print(urls)		
 
 
-def get_exclusions():
-    excluded = []
-    with open('exclusions.csv') as ef:
-        for l in ef.readlines():
-            exclu = l.replace(' \n', '')
-            excluded.append(exclu)
-    return excluded
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth,parser=tweepy.parsers.JSONParser())
 
-def unshorten_url(url):
-    return requests.head(url, allow_redirects=True).url
+#Let's assume those are registred.
+excluded=['twitter.com','facebook.com','fb.me','apple.com','apple.co','snapchat.com','billboard.com','youtube.com','youtu.be','spotify.com','github.com','yahoo.com','fbi.gov','goo.gl','instagram.com','buzzfeed.com','amazon.com','vine.co','twimg.com','persiscope.tv','microsoft.com','fb.on','bit.ly','nike.com']
+accounts=get_accounts()
+lock = threading.Lock()
 
-if __name__ == "__main__":
+for x in range(20):
+	print(x)
+	thread1=myThread(accounts)
+	thread1.daemon=True
+	thread1.start()
 
-    parser = argparse.ArgumentParser()
+while True:
+	time.sleep(1)
+	
 
-    parser.add_argument("-o", "--outfile", dest="output",
-                    help="write results to FILE", metavar="OUTPUT FILE", required=True)
-
-    parser.add_argument("-i", "--infile", dest="input",
-                    help="read twitter accounts from FILE", metavar="INPUT FILE", required=True)
-
-
-    parser.add_argument("-q", "--quiet",
-                    action="store_false", dest="verbose", default=True,
-                    help="don't print status messages to stdout")
-
-    args = parser.parse_args()
-   
-    output = args.output
-    input_file = args.input
-
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
-
-
-    excluded = get_exclusions()
-    accounts = get_accounts(input_file)
-
-    lock = threading.Lock()
-
-    for x in range(20):
-        thread1 = myThread(accounts)
-        thread1.daemon = True
-        thread1.start()
-
-    while True:
-        time.sleep(1)
-
-			
